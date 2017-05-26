b60b716f7e662998619344ef014af5e2c0ab0cea
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-05-26 22:38:47 +0100

    feat: Add search bar with dummy data

--- app/src/main/java/ilyeshammadi/booklib/activities/ListBookActivity.java
+++ app/src/main/java/ilyeshammadi/booklib/activities/ListBookActivity.java
@@ -5,6 +5,14 @@ import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.miguelcatalan.materialsearchview.SearchAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,18 +22,22 @@ import ilyeshammadi.booklib.adapters.ListBookAdapter;
 import ilyeshammadi.booklib.asyntasks.GetListBooksTask;
 import ilyeshammadi.booklib.models.Book;
 
+import static ilyeshammadi.booklib.utils.Constants.TAG;
+
 public class ListBookActivity extends AppCompatActivity {
 
     List<Book> mBooks = new ArrayList<>();
     RecyclerView mListBooksRL;
     ListBookAdapter mAdapter;
-
+    MaterialSearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         // Get the views
         mListBooksRL = (RecyclerView) findViewById(R.id.list_books);
@@ -38,6 +50,74 @@ public class ListBookActivity extends AppCompatActivity {
 
         // Get data from server
         new GetListBooksTask(mAdapter).execute();
+
+
+        // Search view
+        mSearchView = (MaterialSearchView) findViewById(R.id.search_view);
+
+        mSearchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                //Do some magic
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                String strings[] = new String[]{"Hello", "Hallo", "Haaa", "Heeeoo"};
+                mSearchView.setSuggestions(strings);
+                return true;
+            }
+        });
+
+        mSearchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
+
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        mSearchView.setMenuItem(item);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (mSearchView.isSearchOpen()) {
+            mSearchView.closeSearch();
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
